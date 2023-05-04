11c5d951da35422bf4b91cf79d2f91baf3568153
Hafizh Abdul Hafizh Kustiawan <hafizh.abdulhafizhkustiawan@gmail.com>
2023-05-04 12:42:53 +0700

    kedai kopi kenangan esok - search form

--- js/script.js
+++ js/script.js
@@ -6,19 +6,26 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// Toggle class active untuk form
+// Toggle class active untuk search form
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclick = () => {
-    searchForm.classList.toggle('active');
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
 };
 
-//Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+//Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+       searchForm.classList.remove('active');
     }
 });
