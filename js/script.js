9249a0cb1e3154cc0a470bad9573f2a80d1abebc
Hafizh Abdul Hafizh Kustiawan <hafizh.abdulhafizhkustiawan@gmail.com>
2023-05-05 19:58:27 +0700

    search form, shopping cart, -js, html, css

--- js/script.js
+++ js/script.js
@@ -16,16 +16,28 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
-//Klik di luar elemen
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault
+};
+
+// Klik di luar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
-document.addEventListener('click', function(e){
-    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
 
-    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
-       searchForm.classList.remove('active');
-    }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
